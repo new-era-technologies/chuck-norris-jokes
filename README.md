617f0d7fb11ee9b78f6f000e46b69b172ee8d293
Vladymyr Radionov <radionovdon@gmail.com>
2020-05-12 10:20:14 +0300

    full chuck

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# macpaw-msi
-Chuck Norris jokes
+# Chuck Norris jokes
+
+test task msi
+
+See http://msi-chuck.byethost7.com
+
+Instructions
+1. Use Chuck Norris API to get jokes in JSON format - https://api.chucknorris.io/  
+
+2. Ability to get a random joke.
+
+3. Ability to get a random joke from categories (Use API to get all available categories).
+
+4. Ability to get jokes by free text search.
+
+5. Any joke can be marked/unmarked as favourite.
+
+6. Jokes marked as favourite should appear in the right section Favourite.
+
+7. Favourite jokes should be available after reloading the page and stored in the browser
+
+(No need to use Back-end).
+
+Time from updated_at till now (from API)
+The result should be a link to the GitHub with your project.
+
+As a plus - link to the working application.
---
+++ macpaw-msi-may2020/js/script.js
@@ -0,0 +1,417 @@
+'use strict';
+
+
+let newObj;
+const randUrl = 'https://api.chucknorris.io/jokes/random';
+const catUrl = 'https://api.chucknorris.io/jokes/random?category=';
+const srchUrl = 'https://api.chucknorris.io/jokes/search?query=';
+
+const textWrapper = document.getElementById('textMesWrap');
+
+const modalMain = document.getElementById('modal');
+
+
+window.onload = function () {
+    getFromStore();
+    fetchChuckApi('https://api.chucknorris.io/jokes/categories', addCategories);
+    // localStorage.clear();
+}
+
+
+
+
+// fetch json from chucknorris api
+
+function fetchChuckApi(url, funcName) {
+    try {
+        fetch(url,
+            {
+                method: "GET",
+                headers: {
+                    'accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response) => response.json())
+            .then((response) => funcName(addToArr(response)));
+    } catch (error) {
+        alert(error);
+    }
+}
+
+
+function addToArr(obj) {
+
+    let arr = [];
+
+
+    if (obj.id) {
+        document.getElementById(obj.id) !== null ? newObj = {} : newObj = { ...obj };
+    } else if (obj.result) {
+        if (textWrapper.children.length) {
+            obj.result.forEach(val => {
+                if (document.getElementById(val.id) === null) {
+                    arr.push(val);
+                }
+            })
+            newObj = { ...arr };
+        } else {
+            newObj = { ...obj.result };
+        }
+    } else {
+        newObj = { ...obj };
+    }
+
+    return newObj;
+
+}
+
+
+const catCont = document.getElementById('cat_cont');
+
+
+// draw categories
+
+function addCategories(obj) {
+    let catList = document.createElement('ul');
+    catList.classList.add('form__line__inner__list');
+    catCont.appendChild(catList);
+    Object.entries(obj).forEach(([i, val]) => {
+        let catEl = document.createElement('li');
+        catList.appendChild(catEl);
+        let catElInp = document.createElement('input');
+        catElInp.type = 'checkbox';
+        catElInp.id = i;
+        catElInp.name = 'cat';
+        catElInp.value = val;
+        catEl.appendChild(catElInp);
+        let catElLab = document.createElement('label');
+        catElLab.htmlFor = i;
+        catElLab.innerHTML = val;
+        catEl.appendChild(catElLab);
+    })
+}
+
+
+
+
+const randRadInp = document.getElementById('rand');
+const catRadInp = document.getElementById('cat');
+const srchRadInp = document.getElementById('srch');
+const srchTextInp = document.querySelector('.form__line__inner--srch');
+
+
+// additional info by click radio-buts
+
+randRadInp.addEventListener('click', function () {
+    catCont.style.display = 'none';
+    srchTextInp.style.display = 'none';
+});
+catRadInp.addEventListener('click', function () {
+    catCont.style.display = 'block';
+    srchTextInp.style.display = 'none';
+});
+srchRadInp.addEventListener('click', function () {
+    srchTextInp.style.display = 'block';
+    catCont.style.display = 'none';
+});
+
+
+const textSrch = document.getElementById('textSrch');
+
+// check radio buttons
+
+function getSelectedRadioValues(name) {
+    const radios = document.querySelectorAll(`input[name="${name}"]:checked`);
+    radios.forEach((rad) => {
+        if (rad.value === 'rand') {
+            fetchChuckApi(randUrl, addJoke);
+        } else if (rad.value === 'cat') {
+            corCat();
+        } else {
+            corInp();
+        }
+    });
+}
+
+
+// correct radio category
+
+function corCat() {
+    if (getSelectedCheckboxValues('cat').length) {
+        fetchChuckApi(catUrl + getSelectedCheckboxValues('cat'), addJoke);
+    } else {
+        errMes('Chuck dont know anything about this category...');
+    }
+}
+
+
+// correct input text
+
+function corInp() {
+
+    let letters = /^[0-9a-zA-Z]+$/;
+
+    if (2 < textSrch.value.length && textSrch.value.length < 121) {
+        console.log(textSrch.value.length)
+        if (textSrch.value.match(letters)) {
+            fetchChuckApi(srchUrl + textSrch.value, addJokeBySrch);
+        } else {
+            errMes('Chuck using letters and numbers!');
+        }
+    } else {
+        errMes('Chuck using min 3 and max 120 symbols');
+    }
+}
+
+
+
+// check checkbox buttons
+
+function getSelectedCheckboxValues(name) {
+    const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+    let values = [];
+    checkboxes.forEach((cb) => {
+        values.push(cb.value);
+    });
+    return values;
+}
+
+
+const submit = document.getElementById('submit');
+
+// get jokes by submit form
+
+submit.addEventListener('click', function (e) {
+    e.preventDefault();
+    getSelectedRadioValues('jokes');
+});
+
+
+//draw joke
+
+function drawJoke(obj, cont) {
+    let textOuter = document.createElement('div');
+    textOuter.id = obj.id;
+    textOuter.classList.add('textMes__outer_' + cont.id);
+    cont.appendChild(textOuter);
+    for (let i = 0; i < 2; i++) {
+        let textInner = document.createElement('div');
+        textInner.classList.add('textMes__outer_' + cont.id + '__inner_' + obj.id);
+        textOuter.appendChild(textInner);
+    }
+    let comImg = document.createElement('img');
+    comImg.classList.add('textMes__outer_' + cont.id + '__inner__com_img');
+    comImg.src = 'img/com.svg';
+    document.querySelector('.textMes__outer_' + cont.id + '__inner_' + obj.id).appendChild(comImg);
+    let likeImg = document.createElement('a');
+    likeImg.classList.add('textMes__outer_' + cont.id + '__inner__like_img');
+    likeImg.classList.add('textMes__outer_' + cont.id + '__inner__like_img_' + obj.id);
+
+    // check joke in favourites
+    if (localStorage.getItem(obj.id) !== null) {
+        likeImg.classList.add('fav');
+    }
+    likeImg.style.backgroundImage = 'url(img/love.svg)';
+    likeImg.href = '#';
+    document.querySelector('.textMes__outer_' + cont.id + '__inner_' + obj.id).nextElementSibling.appendChild(likeImg);
+    let idBox = document.createElement('div');
+    idBox.classList.add('textMes__outer_' + cont.id + '__inner__idbox');
+    document.querySelector('.textMes__outer_' + cont.id + '__inner_' + obj.id).nextElementSibling.appendChild(idBox);
+    let idText = document.createElement('span');
+    idText.classList.add('textMes__outer_' + cont.id + '__inner__idbox__text');
+    idText.innerHTML = 'ID:';
+    idBox.appendChild(idText);
+    let idLink = document.createElement('a');
+    idLink.classList.add('textMes__outer_' + cont.id + '__inner__idbox__link');
+    idLink.innerHTML = obj.id;
+    idLink.href = obj.url;
+    idLink.target = '_blank';
+    idBox.appendChild(idLink);
+    let idImg = document.createElement('img');
+    idImg.classList.add('textMes__outer_' + cont.id + '__inner__idbox__img');
+    idImg.src = 'img/link.svg';
+    idBox.appendChild(idImg);
+    let jokeText = document.createElement('p');
+    jokeText.classList.add('textMes__outer_' + cont.id + '__inner__joketext');
+    jokeText.innerHTML = obj.value;
+    document.querySelector('.textMes__outer_' + cont.id + '__inner_' + obj.id).nextElementSibling.appendChild(jokeText);
+    let footBox = document.createElement('div');
+    footBox.classList.add('textMes__outer_' + cont.id + '__inner__foot_box')
+    document.querySelector('.textMes__outer_' + cont.id + '__inner_' + obj.id).nextElementSibling.appendChild(footBox);
+    let upd = document.createElement('p');
+    upd.classList.add('textMes__outer_' + cont.id + '__inner__foot_box__update');
+    upd.innerHTML = 'Last update: ' + getHours(Date.parse(obj.updated_at)) + ' hours ago';
+    document.querySelector('.textMes__outer_' + cont.id + '__inner_' + obj.id).nextElementSibling.children[3].appendChild(upd);
+    if (obj.categories != null) {
+        if (obj.categories.length) {
+            let cat = document.createElement('span');
+            cat.classList.add('textMes__outer_' + cont.id + '__inner__foot_box__cat');
+            cat.innerHTML = obj.categories;
+            document.querySelector('.textMes__outer_' + cont.id + '__inner_' + obj.id).nextElementSibling.children[3].appendChild(cat);
+        }
+    }
+
+    remFromFav(obj.id);
+}
+
+
+
+
+
+// adding joke
+
+function addJoke(obj) {
+    if (Object.keys(obj).length) {
+        drawJoke(obj, textWrapper);
+        addToFav(obj.id, obj);
+        remFromFav(obj.id);
+    } else {
+        errMes('Chuck is tyred to do it twice');
+    }
+}
+
+
+// adding joke by searching
+
+function addJokeBySrch(obj) {
+    console.log('addjoke', obj)
+    if (Object.keys(obj).length) {
+        Object.entries(obj).forEach(([k, val]) => {
+            addJoke(val);
+        })
+    } else {
+        errMes('Chuck is very drunk');
+    }
+}
+
+
+// same joke
+
+const popup = document.querySelector('.popup');
+const popText = document.querySelector('.popup__text');
+const popBut = document.querySelector('.popup__but');
+
+function errMes(text) {
+    popText.innerHTML = text;
+    popup.classList.toggle('popup_close');
+    setTimeout(function () { modalMain.classList.toggle('modal_back'); modalMain.classList.toggle('index'); }, 300);
+    popBut.addEventListener('click', function (e) {
+        e.preventDefault();
+        errMes();
+    })
+}
+
+
+
+const favsWrap = document.getElementById('favsWrap');
+
+// add to favourites
+
+function addToFav(id, obj) {
+    const likeOnJokeInMain = document.querySelector('.textMes__outer_textMesWrap__inner__like_img_' + id);
+
+    likeOnJokeInMain.addEventListener('click', function (e) {
+        e.preventDefault();
+        this.classList.toggle('fav');
+        if (localStorage.getItem(id) === null) {
+            addToStore(obj);
+            drawJoke(obj, favsWrap);
+        } else {
+            remUnfav(id);
+            remFromStor(id);
+        }
+    })
+}
+
+// unfavourite joke
+
+function remUnfav(id) {
+    const likeOnJokeInFavs = document.querySelector('.textMes__outer_favsWrap__inner__like_img_' + id);
+    likeOnJokeInFavs.parentElement.parentElement.remove();
+}
+
+
+
+
+// remove from favourites
+
+function remFromFav(id) {
+    const likeOnJokeInFavs = document.querySelector('.textMes__outer_favsWrap__inner__like_img_' + id);
+    const likeOnJokeInMain = document.querySelector('.textMes__outer_textMesWrap__inner__like_img_' + id);
+
+    if (likeOnJokeInFavs instanceof Element) {
+        likeOnJokeInFavs.addEventListener('click', function (e) {
+            e.preventDefault();
+            likeOnJokeInFavs.parentElement.parentElement.remove();
+            if (likeOnJokeInMain instanceof Element) {
+                likeOnJokeInMain.classList.remove('fav');
+            }
+            remFromStor(id);
+        })
+    }
+}
+
+
+// get time
+
+function getHours(ms) {
+    let nMs = Date.now() - ms;
+    let s = nMs / 1000;
+    let m = s / 60;
+    let h = m / 60;
+    return Math.round(h);
+}
+
+
+// favourite menu by clicking
+
+const favMenu = document.getElementById('fav_menu');
+const modalSec = document.querySelector('.modal');
+
+if (window.screen.width < 1440) {
+    favMenu.addEventListener('click', function () {
+        this.children[0].children[0].classList.toggle('line--fir');
+        this.children[0].children[1].classList.toggle('line--sec');
+        modalSec.classList.toggle('modal_overlay');
+        setTimeout(function () { modalMain.classList.toggle('modal_back') }, 300);
+    });
+}
+
+
+// add to localStorage
+
+function addToStore(obj) {
+    let joke = {
+        id: obj.id,
+        url: obj.url,
+        categories: obj.categories.length ? obj.categories : null,
+        value: obj.value,
+        updated_at: obj.updated_at
+    }
+
+    if (typeof (Storage) !== "undefined") {
+        window.localStorage.setItem(joke.id, JSON.stringify(joke));
+    } else {
+        alert('Sorry no storage support');
+    }
+}
+
+
+// remove from localStorage
+
+function remFromStor(id) {
+    window.localStorage.removeItem(id);
+}
+
+
+
+// get favourite jokes from localStorage
+
+function getFromStore() {
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        drawJoke(JSON.parse(value), favsWrap);
+    }
+}
